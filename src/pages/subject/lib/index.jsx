e244e9f84349372e7b3b879d8869598068bbcb7f
吴焯斌 <920658022@qq.com>
2021-05-18 17:17:13 +0800

    feat:fix

--- src/pages/subject/lib/index.jsx
+++ src/pages/subject/lib/index.jsx
@@ -1,73 +1,71 @@
-import { Component } from 'react'
-import { View, ScrollView } from '@tarojs/components'
-import { observer, inject } from 'mobx-react'
-import { Search, Tabbar } from '../../../components/index'
-import './index.scss'
+import { Component } from "react";
+import { View, ScrollView } from "@tarojs/components";
+import { observer, inject } from "mobx-react";
+import { Search, Tabbar } from "../../../components/index";
+import "./index.scss";
 
-@inject('store')
+@inject("store")
 @observer
 class Index extends Component {
-
   state = {
-    activeTab: '本科专业'
-  }
+    activeTab: "本科专业",
+  };
 
-  componentWillMount() { }
+  componentWillMount() {}
 
-  componentDidMount() { }
+  componentDidMount() {}
 
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
-  componentDidShow() { }
+  componentDidShow() {}
 
-  componentDidHide() { }
+  componentDidHide() {}
 
   handleTabChange(activeTab) {
     this.setState({
-      activeTab
-    })
+      activeTab,
+    });
   }
 
   render() {
-    const tabs = ['本科专业', '专科专业']
-    const left = ['法学', '经济学', '教育学', '管理学', '理学']
-    const right = ['财政学类', '经济学类', '金融学类', '其他']
-    const { activeTab } = this.state
+    const tabs = ["本科专业", "专科专业"];
+    const left = ["法学", "经济学", "教育学", "管理学", "理学"];
+    const right = ["财政学类", "经济学类", "金融学类", "其他"];
+    const { activeTab } = this.state;
     return (
-      <View className='b-subject-lib'>
+      <View className="b-subject-lib">
         <Search />
-        <Tabbar tabs={tabs} activeTab={activeTab} onChange={({ target }) => {
-          this.handleTabChange(target)
-        }} />
-        <View className='main'>
-          <ScrollView className='left'>
-            <View className='info'>
-              {
-                left.map((n, i) => (
-                  <View className={`item ${i === 0 && 'selected'}`}>
-                    <View className={`value`}>{n}</View>
-                  </View>
-                ))
-              }
+        <Tabbar
+          tabs={tabs}
+          activeTab={activeTab}
+          onChange={({ target }) => {
+            this.handleTabChange(target);
+          }}
+        />
+        <View className="main">
+          <ScrollView className="left">
+            <View className="info">
+              {left.map((n, i) => (
+                <View className={`item ${i === 0 && "selected"}`}>
+                  <View className={`value`}>{n}</View>
+                </View>
+              ))}
             </View>
           </ScrollView>
-          <ScrollView className='right'>
-            <View className='b-bottom-line title'>经济学</View>
-            <View className='info'>
-              {
-                right.map(n => (
-                  <View className='item'>
-                    <View className='value'>{n}</View>
-                  </View>
-                ))
-              }
+          <ScrollView className="right">
+            <View className="b-bottom-line title">经济学</View>
+            <View className="info">
+              {right.map((n) => (
+                <View className="item">
+                  <View className="value">{n}</View>
+                </View>
+              ))}
             </View>
           </ScrollView>
         </View>
       </View>
-    )
+    );
   }
-
 }
 
-export default Index
+export default Index;
