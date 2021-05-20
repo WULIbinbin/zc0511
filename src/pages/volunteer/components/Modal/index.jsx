2799d8e8fbdc33668738287ae2e8ab568e142de4
吴焯斌 <920658022@qq.com>
2021-05-20 22:01:08 +0800

    feat:fix

--- src/pages/volunteer/components/Modal/index.jsx
+++ src/pages/volunteer/components/Modal/index.jsx
@@ -6,8 +6,18 @@ import SelectNor from "../../../../static/image/select.png";
 import SelectSel from "../../../../static/image/select-sel.png";
 
 export default function ({ echart = null }) {
-  const [folded, setFold] = useState(false);
+  const [folded, setFold] = useState(true);
   const detailTabs = ["艺术型（A）", "企业型（E）", "艺术型（C）"];
+  const details = [
+    {
+      title:'基本特征',
+      desc:'有创造力，乐于创造新颖、与众不同的成果，渴望表现自己的个性，实现自身的价值；做事理想化，追求完美，不重实际。具有一定的艺术才能和个性。善于表达、怀旧、心态较为复杂。'
+    },
+    {
+      title:'典型职业',
+      desc:'有创造力，乐于创造新颖、与众不同的成果，渴望表现自己的个性，实现自身的价值；做事理想化，追求完美，不重实际。具有一定的艺术才能和个性。善于表达、怀旧、心态较为复杂。'
+    },
+  ]
   return (
     <View className="b-vol-modal">
       <View className="b-vol-modal-options">
@@ -47,10 +57,19 @@ export default function ({ echart = null }) {
         <View className="b-vol-modal-content b-top-line">
           <View className="b-vol-modal-detail-tab">
             {detailTabs.map((n) => (
-              <View className="b-vol-modal-detail-tab-item">{n}</View>
+              <View className={`b-vol-modal-detail-tab-item ${n==='艺术型（A）'&&'actived'}`}>{n}</View>
             ))}
           </View>
-          
+          <View className='b-vol-modal-detail-content'>
+              {
+                details.map(n=>(
+                  <>
+                    <View className='b-vol-modal-detail-title'>{n.title}</View>
+                    <View className='b-vol-modal-detail-desc'>{n.desc}</View>
+                  </>
+                ))
+              }
+          </View>
         </View>
       )}
     </View>
--- src/pages/volunteer/recommend/index.jsx
+++ src/pages/volunteer/recommend/index.jsx
@@ -21,7 +21,7 @@ class Index extends Component {
       radar: {
         radius: "65%",
         center: ["50%", "50%"],
-        splitNumber:3,
+        splitNumber: 3,
         name: {
           fontSize: 12,
         },
@@ -79,7 +79,7 @@ class Index extends Component {
 
   render() {
     return (
-      <View bgColor="#f7f7f7">
+      <View className="b-vol-page">
         <VolTitle title="志愿填报推荐" desc="报告单号：CYZY-0000001"></VolTitle>
         <VolTestInfo />
         <VolPreference />
@@ -95,6 +95,18 @@ class Index extends Component {
             />
           }
         ></VolModal>
+        <View className="b-vol-page-bottom-desc">
+          注：由于该理论1959年提出，有些职业在这几十年间发生了很大的变化，甚至已经消失，我们本着尊重全球性著名专家的原则，并未删除或更改，直接展示给测试者。
+        </View>
+        <View className="b-vol-page-button-group">
+          <View className="b-vol-page-button b-vol-page-button-left">
+            <View className="b-vol-page-button-money">￥</View>99.00
+          </View>
+          <View className="b-vol-page-button b-vol-page-button-right">
+            单项解锁
+          </View>
+        </View>
+        <View className='b-vol-page-bottom-example'>看看 示例报告</View>
       </View>
     );
   }
